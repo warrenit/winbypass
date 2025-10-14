facaa73edd72981f8cd14e4b744ec74b694fe47c
warrenit <50739704+warrenit@users.noreply.github.com>
2025-10-14 23:01:39 +0100

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+curl -o C:\Windows\Panther\unattend.xml https://github.com/warrenit/winbypass/blob/7ec54f9bda5f9b683f8fcf11e669ebe5c2434479/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
